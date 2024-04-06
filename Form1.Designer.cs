192215ca349e459112186c23f73257d9da6efa35
Ezequiel Gonzalez <ee.gonzalez.vazquez@gmail.com>
2024-04-05 21:47:38 -0300

    add code to convert

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            browseButton = new Button();
+            fileNameTextBox = new TextBox();
+            timeTextBox = new TextBox();
+            fileNameLabel = new Label();
+            timeLabel = new Label();
+            timePreviewLabel = new Label();
+            convertButton = new Button();
+            SuspendLayout();
+            // 
+            // browseButton
+            // 
+            browseButton.Location = new Point(164, 38);
+            browseButton.Name = "browseButton";
+            browseButton.Size = new Size(75, 23);
+            browseButton.TabIndex = 0;
+            browseButton.Text = "Browse";
+            browseButton.UseVisualStyleBackColor = true;
+            browseButton.Click += browseButton_Click;
+            // 
+            // fileNameTextBox
+            // 
+            fileNameTextBox.Location = new Point(12, 38);
+            fileNameTextBox.Name = "fileNameTextBox";
+            fileNameTextBox.ReadOnly = true;
+            fileNameTextBox.Size = new Size(134, 23);
+            fileNameTextBox.TabIndex = 1;
+            fileNameTextBox.TextChanged += fileNameTextBox_TextChanged;
+            // 
+            // timeTextBox
+            // 
+            timeTextBox.Enabled = false;
+            timeTextBox.Location = new Point(12, 102);
+            timeTextBox.Name = "timeTextBox";
+            timeTextBox.Size = new Size(134, 23);
+            timeTextBox.TabIndex = 2;
+            timeTextBox.TextChanged += timeTextBox_TextChanged;
+            timeTextBox.KeyPress += timeTextBox_KeyPress;
+            // 
+            // fileNameLabel
+            // 
+            fileNameLabel.AutoSize = true;
+            fileNameLabel.Location = new Point(12, 20);
+            fileNameLabel.Name = "fileNameLabel";
+            fileNameLabel.Size = new Size(52, 15);
+            fileNameLabel.TabIndex = 3;
+            fileNameLabel.Text = "Save File";
+            // 
+            // timeLabel
+            // 
+            timeLabel.AutoSize = true;
+            timeLabel.Location = new Point(12, 84);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(104, 15);
+            timeLabel.TabIndex = 4;
+            timeLabel.Text = "Time (in decimals)";
+            // 
+            // timePreviewLabel
+            // 
+            timePreviewLabel.AutoSize = true;
+            timePreviewLabel.Location = new Point(164, 105);
+            timePreviewLabel.Name = "timePreviewLabel";
+            timePreviewLabel.Size = new Size(0, 15);
+            timePreviewLabel.TabIndex = 5;
+            // 
+            // convertButton
+            // 
+            convertButton.Enabled = false;
+            convertButton.Location = new Point(89, 158);
+            convertButton.Name = "convertButton";
+            convertButton.Size = new Size(75, 23);
+            convertButton.TabIndex = 6;
+            convertButton.Text = "Convert";
+            convertButton.UseVisualStyleBackColor = true;
+            convertButton.Click += convertButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(253, 193);
+            Controls.Add(convertButton);
+            Controls.Add(timePreviewLabel);
+            Controls.Add(timeLabel);
+            Controls.Add(fileNameLabel);
+            Controls.Add(timeTextBox);
+            Controls.Add(fileNameTextBox);
+            Controls.Add(browseButton);
+            Name = "Form1";
+            Text = "Time Converter";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button browseButton;
+        private TextBox fileNameTextBox;
+        private TextBox timeTextBox;
+        private Label fileNameLabel;
+        private Label timeLabel;
+        private Label timePreviewLabel;
+        private Button convertButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace HatFileTimeConverter
 {
     public partial class Form1 : Form
@@ -5,6 +7,86 @@ namespace HatFileTimeConverter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string FilePath { get; set; }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "Hat save file (*.hat)|*.hat";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNameTextBox.Text = openFileDialog.SafeFileName;
+                FilePath = openFileDialog.FileName;
+            }
+        }
+
+        private void timeTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.' || (sender as TextBox).Text.IndexOf('.') != -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void timeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(timeTextBox.Text, out var result))
+            {
+                try
+                {
+                    TimeSpan timeSpan = TimeSpan.FromSeconds(result);
+                    timePreviewLabel.Text = timeSpan.ToString("hh':'mm':'ss'.'ff");
+                    convertButton.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    timePreviewLabel.Text = "TOO BIG";
+                    convertButton.Enabled = false;
+                }
+            }
+        }
+
+        private void fileNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            bool flag = string.IsNullOrWhiteSpace(fileNameTextBox.Text);
+            timeTextBox.Enabled = !flag;
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] array = File.ReadAllBytes(FilePath);
+                string @string = Encoding.UTF8.GetString(array);
+                if (@string.Contains("TotalPlayTime"))
+                {
+                    float value = float.Parse(timeTextBox.Text);
+                    byte[] bytes = BitConverter.GetBytes(value);
+                    Array.Copy(bytes, 0, array, 97, 4);
+                    File.WriteAllBytes(FilePath, array);
+                    MessageBox.Show("Time changed successfully.", "Success", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("File does not contain a total play time value. Please open the file in game and save and quit once.", 
+                        "Error", 
+                        MessageBoxButtons.OK, 
+                        MessageBoxIcon.Hand);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File is already in use.",
+                    "Error", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Hand);
+            }
+
         }
     }
 }
